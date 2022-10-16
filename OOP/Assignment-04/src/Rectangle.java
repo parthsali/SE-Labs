c943586ca253a19a4c54b7a20ea77239a4da3aee
Parth Sali <parthsali04@gmail.com>
2022-10-16 22:43:30 +0530

    Assignemnt-04 added

---
+++ OOP/Assignment-04/src/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle extends Shape{
+    void getDim(){
+        System.out.println("Enter Rectangle's Dimension : ");
+        System.out.print("Enter Width : ");
+        base = sc.nextDouble();
+        System.out.print("Enter Height : ");
+        height = sc.nextDouble();
+        System.out.println();
+    }
+
+    @Override
+    void computArea(){
+        System.out.println("Area of Rectangle : " + this.base*this.height + " sq. units " + "\n\n");
+    }
+
+
+}
---
+++ OOP/Assignment-04/src/Triangle.java
@@ -0,0 +1,17 @@
+public class Triangle extends Shape{
+
+    void getDim(){
+        System.out.println("Enter Triangle's Dimension : ");
+        System.out.print("Enter Base : ");
+        base = sc.nextDouble();
+        System.out.print("Enter Height : ");
+        height = sc.nextDouble();
+        System.out.println();
+    }
+
+    @Override
+    void computArea(){
+        System.out.println("Area of Triangle : " + (this.base*this.height)/2  + " sq. units "+ "\n\n" );
+    }
+
+}
